7d527c8f3480f1fef4bb586398c2f2204c1257d9
Alexandre De Assunçao <noowed@Ordinateur-portable-de-Alexandre.local>
2025-04-03 15:56:28 +0200

    déploiement

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/scan-express-site',         // important !
+  assetPrefix: '/scan-express-site/',     // important !
+  images: {
+    unoptimized: true,                    // pour GitHub Pages
+  },
 };
 
 module.exports = nextConfig;
