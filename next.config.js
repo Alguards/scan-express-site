662b6c40084fe078583d0f63223fd07ac5a2ce23
Alexandre De Assunçao <a.deassuncao@altguards.com>
2025-04-04 09:57:40 +0200

    🛠️ Restauration des modifs après déploiement

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/scan-express-site',
+  assetPrefix: '/scan-express-site/',
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
-
